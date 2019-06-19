f109b16cdb9d82958d960e9be2d5ff3183171002
Tythor <tthorin@live.com>
2019-06-19 16:33:21 -0700

    SaltyTeemo

--- src/main/java/com/tythor/saltyteemobot/Bot.java
+++ src/main/java/com/tythor/saltyteemobot/Bot.java
@@ -100,6 +100,15 @@ public class Bot {
 
                 ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
                 configuration = mapper.readValue(is, Configuration.class);
+
+                HashMap<String, String> api = new HashMap<>();
+                api.put("twitch_client_id=", System.getenv("twitch_client_id"));
+                api.put("twitch_client_secret=", System.getenv("twitch_client_secret"));
+                configuration.setApi(api);
+
+                HashMap<String, String> credentials = new HashMap<>();
+                credentials.put("irc", System.getenv("irc"));
+                configuration.setCredentials(credentials);
             } catch (Exception e) {
                 e.printStackTrace();
                 System.out.println("Unable to load Configuration ... Exiting.");
